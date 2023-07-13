730881290812cabee87b3a32bd91c0c6ba75f274
juk <julian.m.krumm@uni-bayreuth.de>
2023-07-13 15:54:42 +0200

    Altered gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -3,12 +3,12 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-sudo reflector -c Switzerland -a 12 --sort rate --save /etc/pacman.d/mirrorlist
-sudo pacman -Sy
+#sudo reflector -c Switzerland -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Syy
 
-sudo firewall-cmd --add-port=1025-65535/tcp --permanent
-sudo firewall-cmd --add-port=1025-65535/udp --permanent
-sudo firewall-cmd --reload
+# sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+# sudo firewall-cmd --add-port=1025-65535/udp --permanent
+# sudo firewall-cmd --reload
 # sudo virsh net-autostart default
 
 
@@ -18,6 +18,6 @@ sudo pacman -S --noconfirm xorg gdm gnome gnome-extra firefox gnome-tweaks simpl
 # sudo flatpak install -y kdenlive
 
 sudo systemctl enable gdm
-/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
-sleep 5
-sudo reboot
+# /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+# sleep 5
+# sudo reboot
